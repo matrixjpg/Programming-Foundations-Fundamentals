ec80cc58265894b4da5734023a9e9c83919cfb3e
Omar El Sayeed <90359498+matrixjpg@users.noreply.github.com>
2021-09-08 18:34:42 -0500

    Add files via upload
    Guessing Game Solution

---
+++ 04_05.py
@@ -0,0 +1,14 @@
+# Assign My Favorite Food To A Variable
+my_favorite_food = "Spaghetti"
+
+# Asking For User Input
+user_favorite_food = input("What's my favorite food? ")
+
+# Checking if the user guess my favorite food right or not
+if user_favorite_food == my_favorite_food:
+    print("Yep! So amazing")
+else:
+    print("Yuck! That's not it!")
+
+# prints out "Thanks for playing" whether the player guess it right or not
+print("Thanks for playing!")
