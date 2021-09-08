561a5dcb64b367525e88b1d5c85db948420f8bab
Omar El Sayeed <90359498+matrixjpg@users.noreply.github.com>
2021-09-08 18:37:50 -0500

    Add files via upload

---
+++ 05_06.py
@@ -0,0 +1,8 @@
+# Defining A function to print out the name variable which passed in argument brackets
+def favorite_city(name):
+    print("One of my favorite cities is: ", name)
+
+# Testing The function with several cases
+favorite_city("Cairo")
+favorite_city("Springfield Gardens")
+favorite_city("Huntsville")
